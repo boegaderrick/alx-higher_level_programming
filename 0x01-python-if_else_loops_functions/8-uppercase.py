25dbfab08b9fc52304581e8c10c2155716de1035
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-11-30 00:54:33 +0000

    convert string to uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
     lower = "abcdefghijklmnopqrstuvwxyz"
-    for letter in str:
-        if letter in lower:
-            print('{:c}'.format(ord(letter) - 32), end='')
-        else:
-            print('{:c}'.format(ord(letter)), end='')
-    print()
+    for l in str:
+        print('{:c}'.format(ord(l) - 32 if l in lower else ord(l)), end='')
+    print(''.format())
