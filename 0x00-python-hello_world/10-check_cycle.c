ca59d4853121c963eca1883f37e0c42ab00fe25a
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-11-30 16:48:45 +0000

    bugfix

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -12,10 +12,23 @@ int check_cycle(listint_t *list)
 	if (!list || !list->next)
 		return (0);
 	ptr = list;
+	
 	ptr2 = list;
 	while (ptr)
 	{
-		/*ptr2 = ptr->next;*/
+		while (ptr2)
+		{
+			if (ptr2 == ptr)
+				return (1);
+			ptr2 = ptr2->next;
+		}
+		ptr = ptr->next;
+	}
+	ptr = list;
+
+	while (ptr)
+	{
+		ptr2 = ptr->next;
 		while (ptr2)
 		{
 			if (ptr2 == ptr)
