b0a68007341ad8379edeba0860a96c2e78688705
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-11-30 13:58:17 +0000

    duplicate string minus specified char

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -3,6 +3,6 @@ def remove_char_at(str, n):
     new_string = ''
     for i in range(len(str)):
         if i is n:
-            i += 1
+            continue
         new_string += (str[i])
     return new_string
