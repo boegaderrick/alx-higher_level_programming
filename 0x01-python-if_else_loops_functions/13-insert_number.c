72cbd89df4e0668ccaad485417a474bc242be592
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-11-30 03:22:11 +0000

    insert node to sorted list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
-* instert_node - inserts node into sorted list
+* insert_node - inserts node into sorted list
 * @head: double pointer to head node of list
 * @number: number to be stored in new node
 * Return: address to new node or NULL of operation failed
@@ -11,8 +11,6 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *ptr, *new_node;
 
 	new_node = malloc(sizeof(listint_t));
-	if (!new_node)
-		return (NULL);
 	new_node->n = number;
 	new_node->next = NULL;
 	ptr = *head;
@@ -21,33 +19,32 @@ listint_t *insert_node(listint_t **head, int number)
 		*head = new_node;
 		return (new_node);
 	}
-	if (!ptr->next)
-	{
-		if (ptr->n < number)
-			ptr->next = new_node;
-		else
-		{
-			new_node->next = ptr;
-			*head = new_node;
-		}
-		return (new_node);
-	}
 	while (ptr)
 	{
+		if (!ptr->next)
+		{
+			if (ptr->n < number)
+				ptr->next = new_node;
+			else
+			{
+				new_node->next = ptr;
+				*head = new_node;
+			}
+			break;
+		}
 		if (ptr->n > number)
 		{
 			new_node->next = ptr;
 			*head = new_node;
-			return (new_node);
+			break;
 		}
 		if ((ptr->n < number && ptr->next->n > number) || ptr->n == number)
 		{
 			new_node->next = ptr->next;
 			ptr->next = new_node;
-			return (new_node);
+			break;
 		}
 		ptr = ptr->next;
 	}
-	ptr = new_node;
 	return (new_node);
 }
