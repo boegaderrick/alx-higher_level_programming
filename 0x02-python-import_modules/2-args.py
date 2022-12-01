7881ab8d1fe3bdf867c8583d6c3dd705d96ace17
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-12-01 13:46:35 +0000

    argument print

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# if __name__ == '__main__':
+import sys
+argv = sys.argv
+argc = len(argv)
+if argc < 2:
+    print('{:d} arguments.'.format(argc - 1))
+elif argc == 2:
+    print('{:d} argument:'.format(argc - 1))
+else:
+    print('{:d} arguments:'.format(argc - 1))
+for i in range(argc):
+    if i < 1:
+        continue
+    print('{:d}: {:s}'.format(i, argv[i]))
