a8fe95e93fa9c4b9741b435a4fd02734dce5b26a
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-12-03 16:04:54 +0000

    create tuple containing length of string and first character

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,5 +4,5 @@ def multiple_returns(sentence):
         first_char = None
     else:
         first_char = sentence[0]
-    my_tuple = len(sentence), sentence[0]
+    my_tuple = len(sentence), first_char
     return my_tuple
