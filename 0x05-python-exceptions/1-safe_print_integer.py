5896b3e12e8b25bf0cf121a26326345facc2b0dc
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-12-19 17:23:18 +0000

    try except ValurError

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,8 @@ def safe_print_integer(value):
     val = True
     try:
         print("{:d}".format(value))
+    except TypeError:
+        val = False
     except ValueError:
         val = False
     return val
