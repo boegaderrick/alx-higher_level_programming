e75fc02823a15bddafdd2169e90d8cc508301a70
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-12-19 21:28:15 +0000

    exception-safe function

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,7 +6,7 @@ def safe_function(fct, *args):
         for a in range(2):
             try:
                 argv.append(args[a])
-            except IndexError as i:
+            except Exception as i:
                 print("Exception: " + str(i), file=sys.stderr)
                 return None
         result = fct(argv[0], argv[1])
