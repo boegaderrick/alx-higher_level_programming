899880e2f84ec54045319efe18827ed6b719741e
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-12-20 20:51:05 +0000

    class instance with exception handling

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,14 +3,15 @@
 
 
 class Square:
-    """initializes Square object
-    Args:
-        size: size of side
-    Exceptions:
-        ValueError: exception indicating size is not of expected value
-        TypeError: exception indicating size is not of expected type
-    """
+    """initializes Square objecti"""
     def __init__(self, size=0):
+        """Square object
+        Args:
+            size: size of side
+        Exceptions:
+            ValueError: exception indicating size is not of expected value
+            TypeError: exception indicating size is not of expected type
+        """
         if isinstance(size, int):
             if size >= 0:
                 self.__size = size
