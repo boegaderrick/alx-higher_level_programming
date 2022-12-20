892ba9b1e83bf6c9b920cf71c0a379d85611c89e
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-12-20 20:31:24 +0000

    getter added

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""square class"""
+
+
+class Square:
+    """initializes Square object"""
+    def __init__(self, size=0):
+        if isinstance(size, int):
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be and integer")
+    """returns area of square"""
+    def area(self):
+        return self.__size**2
