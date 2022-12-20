a2197eaa8d9c66ded7b13e9da0a43b300524fe16
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-12-20 20:15:25 +0000

    class instance with exception handling

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""square class"""
+
+
+class Square:
+    """initializes Square object"""
+    def __init__(self, size=0):
+        if isinstance(size, int):
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError('size must be >= 0')
+        else:
+            raise TypeError('size must be and integer')
