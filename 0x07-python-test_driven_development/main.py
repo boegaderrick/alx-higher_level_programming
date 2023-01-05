39589db597024a6cc6ce8a6e5f0153547949c00f
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-01-05 11:41:59 +0000

    bugfix

--- 0x07-python-test_driven_development/main.py
+++ 0x07-python-test_driven_development/main.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-matrix_mul = __import__('100-matrix_mul').matrix_mul
+matrix_divided = __import__('temp').matrix_divided
 
-#print(matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4]]))
-#print(matrix_mul([[1, 2]], [[3, 4], [5, 6]]))
-print(matrix_mul([[54, 67, 89], [89, 56, 4], [65, 76, 87]], [[47, 85, 99], [34, 2, 6], [87, 76, 8]]))
-print(matrix_mul([[9, 8], [7, 6], [5, 4]], [[1, 2, 3], [4, 5, 6]]))
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6]
+]
+print(matrix_divided(matrix))
+print(matrix)
+
