249aa0ac7b49ecf8dd27201eb0fa031a089f634b
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-01-09 16:30:03 +0000

    square class implementation

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Module contains classes that are connected by inheritance"""
+
+
+class BaseGeometry:
+    """Base geometry class"""
+    def __init__(self):
+        """Class instance instantiation"""
+        pass
+
+    def area(self):
+        """Calculates area"""
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """Validates the value and type of <value>"""
+        if type(value) is not int:
+            raise TypeError(f'{name} must be an integer')
+        if value < 1:
+            raise ValueError(f'{name} must be greater than 0')
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class/BaseGeometry subclass"""
+
+    def __init__(self, width, height):
+        """Rectangle class object instantiation"""
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Calculates area"""
+        try:
+            return self.__width * self.__height
+        except Exception:
+            super().area()
+
+    def __str__(self):
+        """Returns a string implementation of the object fields"""
+        return f'[Rectangle] {self.__width}/{self.__height}'
+
+
+class Square(Rectangle):
+    """Square class derived from Rectangle and BaseGeometry classes"""
+    def __init__(self, size):
+        super(Rectangle, self).integer_validator("size", size)
+        super().__init__(size, size)
+
+    def area(self):
+        """Calculates area"""
+        return super().area()
