00c81f632a99bcebc3d1ae7d2c37bdeb76e189f3
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-01-09 18:42:25 +0000

    setattr

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -6,6 +6,6 @@ def add_attribute(obj, attr, value):
     """Adds an attribute to an object, raises exception if operation failed"""
     imms = [int, float, complex, bool, str, bytes, tuple, frozenset]
     for i in imms:
-        if type(obj) is i or isinstance(obj, i) or issubclass(obj, i):
+        if type(obj) is i or isinstance(obj, i) or issubclass(type(obj), i):
             raise TypeError('can\'t add new attribute')
     setattr(obj, attr, value)
