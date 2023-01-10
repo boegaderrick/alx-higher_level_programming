bb694024f4835da1e9440d454c6422f3472003da
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-01-10 16:34:50 +0000

    load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Module contains script that serializes/deserializes to/from json file"""
+
+
 import sys
+
 load = __import__('6-load_from_json_file').load_from_json_file
 save = __import__('5-save_to_json_file').save_to_json_file
 try:
