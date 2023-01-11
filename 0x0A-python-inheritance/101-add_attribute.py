39b009a369b3c5bf5837bc15674dcdcc4c6ebd34
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-01-11 18:36:28 +0000

    bugfix

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,9 +4,9 @@
 
 def add_attribute(obj, attr, value):
     """Adds an attribute to an object, raises exception if operation failed"""
-    imms = [float, complex, bool, str, bytes, tuple, frozenset]
+    imms = [int, float, complex, bool, str, bytes, tuple, frozenset]
     for i in imms:
-        if type(obj) is i or isinstance(obj, i):
+        if type(obj) is i:
             raise TypeError('can\'t add new attribute')
     if hasattr(obj, '__slots__') and attr not in obj.__slots__:
         raise TypeError('can\'t add new attribute')
