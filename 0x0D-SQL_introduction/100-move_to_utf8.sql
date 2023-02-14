de1642f97c02bb05adff5332ee1b6ba4f87a5fee
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-02-15 02:49:31 +0300

    convert to utf8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,8 @@
 -- This script converts a database to utf8
 
 USE hbtn_0c_0;
-SELECT CONVERT(name USING utf8) FROM first_table;
+
+
+ALTER TABLE second_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE second_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
