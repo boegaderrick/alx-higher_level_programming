139042ea2fac3e85555db4cf8da1e63aa71782ac
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-02-15 19:33:04 +0300

    foreign key

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,7 @@
+-- This script creates a database and a table with non null values.
+-- If any of te two already exist the script won't do anything in that particular case
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL,
+	state_id INT NOT NULL, FOREIGN KEY(state_id) REFERENCES states(id));
