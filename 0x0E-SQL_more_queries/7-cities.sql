7c341efb9b4a1549eea7da467aa57518b524a08c
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-02-15 21:39:09 +0300

    foreign key

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,7 +2,7 @@
 -- If any of the two already exist the script won't do anything in that particular case
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
+-- USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
 id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 name VARCHAR(256) NOT NULL,
