5ce3ea0b940d19608231b0ab04b13634dc5bc0c1
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-02-15 21:28:19 +0300

    foreign key

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,5 +6,5 @@ USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(256) NOT NULL,
-	state_id INT NOT NULL, FOREIGN KEY(state_id) REFERENCES states(id)
+	state_id INT NOT NULL, FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id)
 ) ENGINE=InnoDB;
