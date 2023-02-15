20f416d671e6273196f5e6131616fdb3028d3573
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-02-15 03:25:25 +0300

    temperatures 101

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,5 @@
 -- This script displays average temps based on cities
 
-USE hbtn_0c_0;
-SOURCE temperatures.sql;
+-- USE hbtn_0c_0;
+-- SOURCE temperatures.sql;
 SELECT city, AVG(value) as avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
