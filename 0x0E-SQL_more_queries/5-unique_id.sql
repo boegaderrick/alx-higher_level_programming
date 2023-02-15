6a457a8a5f0732c401b36e721c6632eaab122e35
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-02-15 18:36:49 +0300

    createtable with unique ID

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,3 +1,3 @@
 -- This script creates a table
 
-CREATE TABLE IF NOT EXISTS id_not_null(id INT PRIMARY KEY, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id(id INT PRIMARY KEY, name VARCHAR(256));
