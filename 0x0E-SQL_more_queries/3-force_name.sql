fbc5af23740546da00adb07fba3519d9b2a3d01f
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-02-15 17:57:41 +0300

    create table

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,3 +1,3 @@
 -- This script creates a table
 
-CREATE TABLE IF NOT EXISTS force_name(id INT PRIMARY KEY, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS force_name(id INT PRIMARY KEY, name VARCHAR(256) NOT NULL);
