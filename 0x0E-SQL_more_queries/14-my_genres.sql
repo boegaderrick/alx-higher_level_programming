29606c00727eb15a0850c7b05ee57f1efb3d8979
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-02-16 03:41:31 +0300

    list genres associated with a show

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,6 +1,6 @@
 -- This script shows all genres associated with a show
 
-SET @s = (SELECT id FROM tv_shows WHERE title = 'Dexter');
+-- SET @s = (SELECT id FROM tv_shows WHERE title = 'Dexter');
 SELECT tv_genres.name FROM tv_genres JOIN tv_show_genres
-ON tv_genres.id = tv_show_genres.genre_id WHERE tv_show_genres.show_id = @s
+ON tv_genres.id = tv_show_genres.genre_id WHERE tv_show_genres.show_id = 8
 ORDER BY tv_genres.name ASC;
