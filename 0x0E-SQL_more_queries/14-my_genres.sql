c0be5ea342a3066f8d4cc6594efa226554843d8d
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-02-16 03:39:44 +0300

    list genres associated with a show

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,4 +2,5 @@
 
 SET @s = (SELECT id FROM tv_shows WHERE title = 'Dexter');
 SELECT tv_genres.name FROM tv_genres JOIN tv_show_genres
-ON tv_genres.id = tv_show_genres.genre_id WHERE tv_show_genres.show_id = @s;
+ON tv_genres.id = tv_show_genres.genre_id WHERE tv_show_genres.show_id = @s
+ORDER BY tv_genres.name ASC;
