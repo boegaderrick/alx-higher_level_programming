95938905a87efd16b9695777e7febdd24302a179
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-03-15 00:13:22 +0300

    js objects

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,12 +1,18 @@
 #!/usr/bin/node
 /* This script reads two files and writes content to third file */
-const fs = require('fs');
+const fs = require('fs').promises;
 
-for (let i = 3; i > 1; i--) {
-  fs.readFile(process.argv[i], 'utf-8', (err, content) => {
+/*
+for (let i = 2; i < 4; i--) {
+  fs.readFile(process.argv[i], 'utf-8', (err, data) => {
     if (err) {
       console.error(err);
     }
-    fs.appendFile(process.argv[4], content, () => {});
+    fs.writeFile(process.argv[4], data, { flag: 'a' }, () => {});
   });
-}
+} */
+
+const src = [process.argv[2], process.argv[3]];
+
+Promise.all(src.map(x => fs.readFile(x, 'utf-8')))
+  .then(data => fs.writeFile(process.argv[4], data.join('')));
