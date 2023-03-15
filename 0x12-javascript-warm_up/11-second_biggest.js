b593fd152ec69c322fd87ca31fc99410e0b322e3
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-03-15 19:46:41 +0300

    javascript intro

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,22 @@
 #!/usr/bin/node
+/* This script finds second largest number of arguments passed */
 
-const arr = process.argv.slice(2).sort().reverse();
+const arr = process.argv.slice(2);
 if (arr.length < 2) {
   console.log(0);
 } else {
-  console.log(arr[1]);
+  let largest = parseInt(arr[0]);
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = parseInt(arr[i]);
+    if (arr[i] > largest) {
+      largest = arr[i];
+    }
+  }
+  let sLargest = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > sLargest && arr[i] < largest) {
+      sLargest = arr[i];
+    }
+  }
+  console.log(sLargest);
 }
