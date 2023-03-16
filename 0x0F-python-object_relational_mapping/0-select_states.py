1b16d031caa0b2cf9184a4ba03591732a1efad5b
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-03-16 20:26:41 +0300

    python ORM

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """This script shows values in a db using mysqldb module"""
-import sys
-import MySQLdb
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
 
-h = 'localhost'
-p = 3306
-u = sys.argv[1]
-pw = sys.argv[2]
-d = sys.argv[3]
-db = MySQLdb.connect(host=h, port=p, user=u, passwd=pw, db=d)
-cursor = db.cursor()
-cursor.execute('SELECT * FROM states ORDER BY states.id')
-print('\n'.join(str(r) for r in cursor.fetchall()))
-cursor.close()
-db.close()
+    h = 'localhost'
+    p = 3306
+    u = sys.argv[1]
+    pw = sys.argv[2]
+    d = sys.argv[3]
+    db = MySQLdb.connect(host=h, port=p, user=u, passwd=pw, db=d)
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM states ORDER BY states.id')
+    print('\n'.join(str(r) for r in cursor.fetchall()))
+    cursor.close()
+    db.close()
