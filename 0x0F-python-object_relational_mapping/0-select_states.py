34d34828b293a7b41f9e21f71d98ca97421147b3
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-03-17 08:59:32 +0300

    python ORM

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
 """This script shows values in a database by means of mysqldb module"""
-if __name__ == '__main__':
-    import sys
-    import MySQLdb
+import sys
+import MySQLdb
 
-    h = 'localhost'
-    p = 3306
-    u = sys.argv[1]
-    pw = sys.argv[2]
-    d = sys.argv[3]
-    try:
-        db = MySQLdb.connect(host=h, port=p, user=u, passwd=pw, db=d)
-        cursor = db.cursor()
-        cursor.execute('SELECT * FROM states ORDER BY states.id ASC')
-        print('\n'.join(str(r) for r in cursor.fetchall()))
-        cursor.close()
-        db.close()
-    except (MySQLdb.Error):
-        pass
+h = 'localhost'
+p = 3306
+u = sys.argv[1]
+pw = sys.argv[2]
+d = sys.argv[3]
+try:
+    db = MySQLdb.connect(host=h, port=p, user=u, passwd=pw, db=d)
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM states ORDER BY states.id ASC')
+    print('\n'.join(str(r) for r in cursor.fetchall()))
+    cursor.close()
+    db.close()
+except (MySQLdb.Error):
+    pass
