c0ff065d1c127d9e189897f52875290c072a01b1
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-03-18 16:29:15 +0300

    python ORM - state class

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python3
-"""This module define a class mapped to a db hello world hello world"""
-import SQLAlchemy
-import MySQLdb
+"""This module define a class mapped to a db"""
 import sys
-from sqlalchemy import create_engine, Sequence
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Integer, String, Column
 
-host = 'localhost'
+"""host = 'localhost'
 port = 3306
 user = sys.argv[1]
 password = sys.argv[2]
 database = sys.argv[3]
 
 string = f'mysql+mysqldb://{user}:{password}@{host}:{port}/{database}'
-engine = create_engine(string)
+engine = create_engine(string)"""
 Base = declarative_base()
 
 
 class State(Base):
-    """State class definition hello world hello world hello world"""
+    """State class definition"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
