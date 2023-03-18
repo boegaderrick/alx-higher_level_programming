9c1741c96da54b884d0fe4823a06a4b199168e26
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-03-18 16:59:01 +0300

    python ORM - db query

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -3,21 +3,22 @@
     This script queries state name and id from a database
     The results are printed in a custom format
 """
-from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
-import sys
+if __name__ == '__main__':
+    from model_state import Base, State
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import create_engine
+    import sys
 
-host = 'localhost'
-port = 3306
-user = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
+    host = 'localhost'
+    port = 3306
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-string = f'mysql+mysqldb://{user}:{password}@{host}:{port}/{database}'
-engine = create_engine(string)
-Session = sessionmaker(bind=engine)
-session = Session()
-states = session.query(State.id, State.name).order_by(State.id)
-for state in states:
-    print(str(state[0]) + ': ' + str(state[1]))
+    string = f'mysql+mysqldb://{user}:{password}@{host}:{port}/{database}'
+    engine = create_engine(string)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State.id, State.name).order_by(State.id)
+    for state in states:
+        print(str(state[0]) + ': ' + str(state[1]))
