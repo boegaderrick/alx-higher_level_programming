49e9bd83bd883df15738eac9082e3e57af6d34c4
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-03-18 18:43:17 +0300

    python ORM - delete records

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    This script deletes records that meet a specified criteria
+"""
+if __name__ == '__main__':
+    from model_state import Base, State
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import create_engine
+    import sys
+
+    host = 'localhost'
+    port = 3306
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    string = f'mysql+mysqldb://{user}:{password}@{host}:{port}/{database}'
+    engine = create_engine(string)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).filter(State.name.ilike('%a%'))
+    for state in states:
+        session.delete(state)
+    session.commit()
