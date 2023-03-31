a1e4d3b627ac5b20e75dfcc928b59820d53da80c
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-03-31 20:36:20 +0300

    urllib post request

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     url = sys.argv[1]
     """data = f'email={sys.argv[2]}'.replace('@', '%40')"""
     data = urlencode({'email': sys.argv[2]})
-    data = data.encode()
+    data = data.encode('utf-8')
     req = Request(url, data)
     with urlopen(req) as response:
-        print(response.read().decode())
+        print(response.read().decode('utf-8'))
