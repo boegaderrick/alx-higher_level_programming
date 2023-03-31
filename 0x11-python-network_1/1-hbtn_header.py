644d46d577d1279a45b1a38f979282737c22040c
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-03-31 18:44:55 +0300

    urllib fetch and get header variable

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This script fetches a url and prints a header value"""
-import urllib.request
-import sys
-with urllib.request.urlopen(sys.argv[1]) as response:
-    print(response.info().get('X-Request-Id'))
+if __name__ == '__main__':
+    from urllib.request import urlopen
+    import sys
+    with urlopen(sys.argv[1]) as response:
+        print(response.info().get('X-Request-Id'))
