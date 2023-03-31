b8f2b72373d5309269ba90ba0e4af1b6b5412265
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-03-31 19:39:51 +0300

    urllib post request

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    This script posts data to a url
+"""
+if __name__ == '__main__':
+    from urllib.request import urlopen, Request
+    import sys
+    url = sys.argv[1]
+    data = f'email={sys.argv[2]}'.replace('@', '%40')
+    data = data.encode('ascii')
+    req = Request(url, data)
+    with urlopen(req) as response:
+        print(response.read().decode())
