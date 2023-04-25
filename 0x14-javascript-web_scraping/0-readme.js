e4b677ff0cc1a32af5f632c3728877355b5f1597
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-04-25 17:15:18 +0300

    nodejs file I/O

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const fs = require('fs');
+const argv = require('process').argv;
+
+fs.readFile(argv[2], 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(data);
+});
